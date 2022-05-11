1e69cf7c11fdf53a5db33dd7cbdbf4b9812357ea
Anurag <yadavanurag307@gmail.com>
2022-05-11 15:21:30 +0530

    Equals to 42

---
+++ src/com/anurag/Naam.java
@@ -0,0 +1,17 @@
+package com.anurag;
+
+public class Naam {
+    public static void main(String[] args) {
+
+        String name = "Anurag Yadav";
+        changeName(name);
+
+        System.out.println(name);
+    }
+
+  static void changeName(String naam) {
+        naam = "Abhishek Yadav";
+    }
+
+
+}
